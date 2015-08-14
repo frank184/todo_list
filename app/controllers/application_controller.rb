62b0b6163bf6cc81a9c23ff4389e386f2f27fd0a
frank184 <francoisbelanger1993@hotmail.com>
2015-08-14 07:50:18 -0400

    Added conditional task/new fetching with ajax & bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def bootstrap_class_for flash_type
+    case flash_type
+    when 'success'
+        "alert-success"
+      when 'error'
+        "alert-error"
+      when 'alert'
+        "alert-danger"
+      when 'notice'
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
+  
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,11 +5,7 @@ class TasksController < ApplicationController
   # GET /tasks.json
   def index
     @tasks = Task.all
-  end
-
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
+    console
   end
 
   # GET /tasks/new
@@ -17,24 +13,22 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
-  def edit
-  end
-
   # POST /tasks
   # POST /tasks.json
   def create
-    @task = Task.new(task_params)
-    if @task.save
+    @task = Task.new(new_task_params)
+    @success = @task.save
+    if @success
       flash.now[:notice] = 'Task was successfully created.'
     else
+      flash.now[:alert] = 'Task was not successfully created.'
     end
   end
 
   # PATCH/PUT /tasks/1
   # PATCH/PUT /tasks/1.json
   def update
-    if @task.update(task_params)
+    if @task.update(update_task_params)
       flash.now[:notice] = 'Task was successfully updated.'
     else
     end
@@ -48,13 +42,15 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def task_params
+    def new_task_params
       params.require(:task).permit(:title)
+    end
+
+    def update_task_params
+      params.require(:task).permit(:complete)
     end
 end
