b093817bd281b0e20c7ad1af2f2add2111d9dff5
frank184 <francoisbelanger1993@hotmail.com>
2015-09-19 05:29:36 +0000

    removed Task :title titelize

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,11 @@
 class Task < ActiveRecord::Base
-  
+
   belongs_to :user
 
   validates :title, presence: true
 
   def title=(value)
-    write_attribute(:title, value.titleize)
+    write_attribute(:title, value)
   end
 
 end
